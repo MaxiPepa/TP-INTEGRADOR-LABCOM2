78d05e05bc25422be7f3a6dabc878fbff7192507
MaxiPepa <maximopepa@gmail.com>
2022-06-11 14:41:48 -0300

    Movida función de getCitiesFromLocalStorage a common.js

--- js/pages/add-city.js
+++ js/pages/add-city.js
@@ -1,14 +1,3 @@
-function getCitiesFromLocalStorage() {
-    let cities = localStorage.getItem("CITIES");
-
-    if (cities) {
-        cities = JSON.parse(cities);
-    } else {
-        cities = [];
-    }
-    return cities;
-}
-
 function validateCity(newCity) {
     let cities = getCitiesFromLocalStorage()
   
